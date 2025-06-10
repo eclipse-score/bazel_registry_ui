17b81b13f44d5256a6b7b5108ea5a7c0a60e2b07
Aymen-Soussi-01 <aymen.soussi@expleogroup.com>
2025-06-10 15:15:01 +0200

    Revert changes

--- next.config.js
+++ next.config.js
@@ -2,10 +2,11 @@
 const nextConfig = {
   output: 'export',
   reactStrictMode: true,
-  // 👇 Required for GitHub Pages under /bcr-ui
-  basePath: '/bcr-ui',
-  assetPrefix: '/bcr-ui',
+  // for hosting under GitHub pages
+  basePath: '',
+  // Increase timeout for generating static pages from default 60s to avoid issues like:
+  // Restarted static page generation for /modules/xxx/y.y.y because it took more than 60 seconds
   staticPageGenerationTimeout: 600,
 }
 
-module.exports = nextConfig;
+module.exports = nextConfig
