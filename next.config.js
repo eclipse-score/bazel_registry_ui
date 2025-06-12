8bcd6ff682319e70f70229bfccf5f0a0ee046f6b
Aymen-Soussi-01 <aymen.soussi@expleogroup.com>
2025-06-12 16:55:33 +0200

    Correct URL and update deploy-ui script

--- next.config.js
+++ next.config.js
@@ -3,8 +3,8 @@ const nextConfig = {
   output: 'export',
   reactStrictMode: true,
   // 👇 Required for GitHub Pages under /bcr-ui
-  basePath: '/bcr-ui',
-  assetPrefix: '/bcr-ui',
+  basePath: '/bazel_registry_ui',
+  assetPrefix: '/bazel_registry_ui',
   staticPageGenerationTimeout: 600,
 }
 
