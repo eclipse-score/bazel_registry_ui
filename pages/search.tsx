8f95932c8c37edf137cd3e51acfab53676442604
Dan Calavrezo <195309321+dcalavrezo-qorix@users.noreply.github.com>
2025-10-03 06:13:12 -0700

    Reapply "feat: show a warning for deprecated or archived modules" (#204) (#206)

--- pages/search.tsx
+++ pages/search.tsx
@@ -83,12 +83,28 @@ const Search: NextPage<SearchPageProps> = ({ searchIndex }) => {
             <h2 className="font-bold text-lg">Search results</h2>
             <div className="grid grid-cols-1 md:grid-cols-2 gap-8 mt-4">
               {searchResults && searchResults.length ? (
-                searchResults.map(({ module, version, hasAttestationFile }) => (
-                  <ModuleCard
-                    key={module}
-                    {...{ module, version, hasAttestationFile }}
-                  />
-                ))
+                searchResults.map(
+                  ({
+                    module,
+                    version,
+                    hasAttestationFile,
+                    isArchived,
+                    deprecated,
+                    deprecationMessage,
+                  }) => (
+                    <ModuleCard
+                      key={module}
+                      {...{
+                        module,
+                        version,
+                        hasAttestationFile,
+                        isArchived,
+                        deprecated,
+                        deprecationMessage,
+                      }}
+                    />
+                  )
+                )
               ) : (
                 <div className="text-gray-600">
                   <p>
